eead06ba7743457f8ed8cae95079462ce1e4e7b9
Abdelrahman Omar <abd.hanyomar3599@gmail.com>
2025-12-06 14:11:04 +0200

    only Loss_MSE class definitions

--- lib3/losses.py
+++ lib3/losses.py
@@ -13,12 +13,3 @@ class Loss_MSE(Loss):
         self.dinputs = -2 * (y_true - y_pred) / outputs
         self.dinputs = self.dinputs / samples
         return self.dinputs
-    
-class SGD:
-    def __init__(self, learning_rate=0.1):
-        self.learning_rate = learning_rate
-
-    def update_params(self, layers):
-        for layer in layers:
-            layer.weights -= self.learning_rate * layer.dweights
-            layer.biases -= self.learning_rate * layer.dbiases
