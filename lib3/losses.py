7fa6e9240fc72538cfea413623b816174901687d
Abdelrahman Omar <abd.hanyomar3599@gmail.com>
2025-12-06 14:06:15 +0200

    Implement MSE and SGD classes in losses.py

---
+++ lib3/losses.py
@@ -0,0 +1,24 @@
+class Loss:
+    pass    
+
+class Loss_MSE(Loss):
+    def forward(self, output, y):
+        sample_losses = np.mean((y - output) ** 2, axis=-1)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+    def backward(self, y_pred, y_true):
+        samples = len(y_pred)
+        outputs = len(y_pred[0])
+        self.dinputs = -2 * (y_true - y_pred) / outputs
+        self.dinputs = self.dinputs / samples
+        return self.dinputs
+    
+class SGD:
+    def __init__(self, learning_rate=0.1):
+        self.learning_rate = learning_rate
+
+    def update_params(self, layers):
+        for layer in layers:
+            layer.weights -= self.learning_rate * layer.dweights
+            layer.biases -= self.learning_rate * layer.dbiases
