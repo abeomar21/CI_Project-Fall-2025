59f4cf78e4c474c17a8101096af1292d823c6d5e
abeomar21 <abd.hanyomar3599@gmail.com>
2025-12-06 18:02:57 +0200

    updated core library components (Activation, losses)

--- lib3/activations.py
+++ lib3/activations.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Activation:
     pass
 
@@ -12,7 +13,7 @@ class Activation_Tanh(Activation):
 
     def backward(self, dvalues):
         # Derivative of tanh: 1 - tanh(x)^2
-        self.dinputs = dvalues * (1 - self.output ** 2)
+        self.dinputs = dvalues * (1 - self.output**2)
         return self.dinputs
 
 
@@ -38,30 +39,3 @@ class Activation_ReLU(Activation):
         self.dinputs = dvalues.copy()
         self.dinputs[self.inputs <= 0] = 0
         return self.dinputs
-
-class Activation_Softmax:
-    def forward(self, inputs):
-        # Remember input values for backward pass
-        self.inputs = inputs
-        # Subtract max value for numerical stability
-        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        # Normalize the values to get probabilities
-        self.output = exp_values / np.sum(exp_values, axis=1, keepdims=True)
-        return self.output
-
-    def backward(self, dvalues):
-        # Create uninitialized gradient array
-        self.dinputs = np.empty_like(dvalues)
-
-        # Loop over samples
-        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
-            # Flatten output array
-            single_output = single_output.reshape(-1, 1)
-            # Calculate Jacobian matrix
-            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
-            # Calculate sample gradient
-            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
-
-        return self.dinputs
-
-
--- lib3/losses.py
+++ lib3/losses.py
@@ -1,12 +1,16 @@
+import numpy as np
+
+
 class Loss:
-    pass    
+    pass
+
 
 class Loss_MSE(Loss):
     def forward(self, output, y):
         sample_losses = np.mean((y - output) ** 2, axis=-1)
         data_loss = np.mean(sample_losses)
         return data_loss
-    
+
     def backward(self, y_pred, y_true):
         samples = len(y_pred)
         outputs = len(y_pred[0])
