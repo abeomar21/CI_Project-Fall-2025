b75c6062a3e015f7544a218ee712b30481baf55d
Abdelrahman Omar <abd.hanyomar3599@gmail.com>
2025-12-06 13:47:21 +0200

    Implement activation functions: Tanh, Sigmoid, ReLU

---
+++ lib3/activations.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+# Base class (optional)
+class Activation:
+    pass
+
+
+class Activation_Tanh(Activation):
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.tanh(inputs)
+        return self.output
+
+    def backward(self, dvalues):
+        # Derivative of tanh: 1 - tanh(x)^2
+        self.dinputs = dvalues * (1 - self.output ** 2)
+        return self.dinputs
+
+
+class Activation_Sigmoid(Activation):
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = 1 / (1 + np.exp(-inputs))
+        return self.output
+
+    def backward(self, dvalues):
+        sigmoid_deriv = self.output * (1 - self.output)
+        self.dinputs = dvalues * sigmoid_deriv
+        return self.dinputs
+
+
+class Activation_ReLU(Activation):
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.maximum(0, inputs)
+        return self.output
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
+        return self.dinputs
