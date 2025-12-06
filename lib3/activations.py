4fd3096620ec5c7ee70d79603b31d5b1df2afabe
Abdelrahman Omar <abd.hanyomar3599@gmail.com>
2025-12-06 14:00:29 +0200

    Implement Activation_Softmax class
    Added Softmax activation class with forward and backward methods.

--- lib3/activations.py
+++ lib3/activations.py
@@ -39,3 +39,29 @@ class Activation_ReLU(Activation):
         self.dinputs[self.inputs <= 0] = 0
         return self.dinputs
 
+class Activation_Softmax:
+    def forward(self, inputs):
+        # Remember input values for backward pass
+        self.inputs = inputs
+        # Subtract max value for numerical stability
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        # Normalize the values to get probabilities
+        self.output = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        return self.output
+
+    def backward(self, dvalues):
+        # Create uninitialized gradient array
+        self.dinputs = np.empty_like(dvalues)
+
+        # Loop over samples
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+            # Calculate Jacobian matrix
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            # Calculate sample gradient
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
+        return self.dinputs
+
+
